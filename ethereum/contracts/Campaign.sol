df9e49f6acf71a566ab4c67542b9a78d4ea0090c
ayansome1 <ayansome1@gmail.com>
2021-04-28 15:34:25 +0530

    init

---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+    function createCampaign(uint minimum) public{
+        address newCampaign = address(new Campaign(minimum, msg.sender));
+        deployedCampaigns.push(newCampaign);
+    }
+    
+    function getDeployedCampaigns() public view returns(address [] memory) {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address payable recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+    // Reference1: https://docs.soliditylang.org/en/v0.7.0/types.html?highlight=struct#structs 
+    // Reference2: https://ethereum.stackexchange.com/questions/87451/solidity-error-struct-containing-a-nested-mapping-cannot-be-constructed/97883#97883
+    
+    uint numRequests;
+    mapping (uint => Request) public requests;
+    
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+    
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    constructor(uint minimum, address creator) {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+    
+    function contribute() public payable {
+        require(msg.value>minimumContribution);
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+    
+    function createRequest(string memory description, uint value, address payable recipient) public restricted {
+        Request storage newRequest = requests[numRequests++];
+        newRequest.description = description;
+        newRequest.value = value;
+        newRequest.recipient = recipient;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;
+        // NOTE: no need to initialize approvals as its not value type.Its an address reference type
+    }
+    
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+    
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+}
